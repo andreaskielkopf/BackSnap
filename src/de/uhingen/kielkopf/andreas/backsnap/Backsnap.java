49c78c152592058fd84a9d9a8cebad2baf9f529e
Andreas Kielkopf <andreas.kielkopf@gmail.com>
2024-02-26 18:03:16 +0100

    Auswahl über Label ermöglichen per Map

--- src/de/uhingen/kielkopf/andreas/backsnap/Backsnap.java
+++ src/de/uhingen/kielkopf/andreas/backsnap/Backsnap.java
@@ -59,7 +59,7 @@ public class Backsnap {
    static final Flag                   ECLIPSE        =new Flag('z', "eclipse");
    static final Flag                   PEXEC          =new Flag('p', "pexec");                  // use pexec instead of sudo
    static public final String          SNAPSHOT       ="snapshot";
-   static public final String          BS_VERSION     ="BackSnap Version 0.6.8.1 (2024/02/23)";
+   static public final String          BS_VERSION     ="BackSnap Version 0.6.8.2 (2024/02/26)";
    static public final String          LF             =System.lineSeparator();
    static public void main(String[] args) {
       Flag.setArgs(args, "");
